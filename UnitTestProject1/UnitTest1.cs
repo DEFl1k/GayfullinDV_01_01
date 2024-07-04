2397c0838a404134112b57a03083fee3fa8fa658
DEFl1k <123548947+DEFl1k@users.noreply.github.com>
2024-07-04 10:55:52 +0500

    Update UnitTest1.cs

--- UnitTestProject1/UnitTest1.cs
+++ UnitTestProject1/UnitTest1.cs
@@ -1,15 +1,15 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
-using static GayfullinDV_01_01.Program;
 
 namespace UnitTestProject1
 {
     [TestClass]
     public class UnitTest1
     {
+       
         [TestMethod]
         public void TestMethod1()
         {
-            Pitanie pitanie = new Pitanie();
+            GayfullinDV_01_01.Pitanie pitanie = new GayfullinDV_01_01.Pitanie();
             pitanie.Belok = 100.5;
             pitanie.Uglevods = 25;
             Assert.AreEqual(pitanie.Uglevods * 4 + pitanie.Belok * 4, pitanie.Q());
@@ -18,12 +18,13 @@ namespace UnitTestProject1
         [TestMethod]
         public void TestMethod2()
         {
-            Potomok potomok = new Potomok();
+            GayfullinDV_01_01.Potomok potomok = new GayfullinDV_01_01.Potomok();
             potomok.Belok = 100.5;
             potomok.Uglevods = 25;
             potomok.Zhiri = 73.56;
 
             Assert.AreEqual(potomok.Q() * 1.5 + potomok.Zhiri * 10, potomok.Qp());
         }
+
     }
 }
