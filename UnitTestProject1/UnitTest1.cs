3e7758cdbfa383b082350372e4f9f3668394138d
DEFl1k <123548947+DEFl1k@users.noreply.github.com>
2024-07-04 11:30:34 +0500

    Update UnitTest1.cs

--- UnitTestProject1/UnitTest1.cs
+++ UnitTestProject1/UnitTest1.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using GayfullinDV_01_01;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using static GayfullinDV_01_01.Program;
 
 namespace UnitTestProject1
 {
@@ -9,8 +11,8 @@ namespace UnitTestProject1
         [TestMethod]
         public void TestMethod1()
         {
-            GayfullinDV_01_01.Pitanie pitanie = new GayfullinDV_01_01.Pitanie();
-            pitanie.Belok = 100.5;
+            Pitanie pitanie = new Pitanie();
+            pitanie.Belok = 100;
             pitanie.Uglevods = 25;
             Assert.AreEqual(pitanie.Uglevods * 4 + pitanie.Belok * 4, pitanie.Q());
         }
@@ -18,13 +20,23 @@ namespace UnitTestProject1
         [TestMethod]
         public void TestMethod2()
         {
-            GayfullinDV_01_01.Potomok potomok = new GayfullinDV_01_01.Potomok();
-            potomok.Belok = 100.5;
-            potomok.Uglevods = 25;
-            potomok.Zhiri = 73.56;
+            Potomok potomok = new Potomok();
+            potomok.Belok = 24.5;
+            potomok.Uglevods = 78;
+            potomok.Zhiri = 26.27;
 
             Assert.AreEqual(potomok.Q() * 1.5 + potomok.Zhiri * 10, potomok.Qp());
         }
 
+        [TestMethod]
+        public void TestMethod3()
+        {
+            Pitanie pitanie = new Pitanie();
+            pitanie.Belok = 692.15;
+            pitanie.Uglevods = 8;
+            pitanie.Name = "Молоко";
+
+            Assert.AreEqual($"Название: Молоко, Кол-во белка: 692,15г, Кол-во углеводов: 8г, Q: 2800,6", pitanie.Info());
+        }
     }
 }
