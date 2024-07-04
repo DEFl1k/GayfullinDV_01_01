b2c542d3ed25ec5259c6e68bc89256a730e3552c
murad <murad231190@mail.ru>
2024-07-04 10:47:37 +0500

    first commit

---
+++ Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+
+namespace GayfullinDV_01_01
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Potomok product = new Potomok();
+            Console.WriteLine("Введите название");
+            product.Name = Console.ReadLine();
+            Console.WriteLine("Введите кол-во белка");
+            product.Belok = double.Parse(Console.ReadLine());
+            Console.WriteLine("Введите кол-во жиров");
+            product.Zhiri = double.Parse(Console.ReadLine());
+            Console.WriteLine("Введите кол-во углеводов");
+            product.Uglevods = double.Parse(Console.ReadLine());
+
+            Console.WriteLine(product.Info());
+
+            Console.ReadKey();
+        }
+
+        public class Pitanie
+        {
+            public string Name { get; set; }
+            public double Belok { get; set; }
+            public double Uglevods { get; set; }
+
+            public double Q()
+            {
+                return Uglevods * 4 + Belok * 4;
+            }
+
+            public virtual string Info()
+            {
+                return $"Название: {Name}, Кол-во белка: {Belok}г, Кол-во углеводов: {Uglevods}г, Q: {Q()}";
+            }
+        }
+
+        public class Potomok : Pitanie
+        {
+            public double Zhiri { get; set; }
+
+            public double Qp()
+            {
+                if (Zhiri >= 0 && Zhiri <= 10)
+                {
+                    return Q() * 1.2 + Zhiri * 9;
+                }
+
+                else if (Zhiri > 10)
+                {
+                    return Q() * 1.5 + Zhiri * 10;
+                }
+                return 0;
+            }
+
+            public override string Info()
+            {
+                return  $"Название: {Name}, Кол-во белка: {Belok}г, Кол-во углеводов: {Uglevods}г, Q: {Q()}, Кол-во жиров: {Zhiri}г, Qp: {Qp()}";
+            }
+        }
+    }
+}
---
+++ UnitTestProject1/UnitTest1.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using static GayfullinDV_01_01.Program;
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            Pitanie pitanie = new Pitanie();
+            pitanie.Belok = 100.5;
+            pitanie.Uglevods = 25;
+            Assert.AreEqual(pitanie.Uglevods * 4 + pitanie.Belok * 4, pitanie.Q());
+        }
+
+        [TestMethod]
+        public void TestMethod2()
+        {
+            Potomok potomok = new Potomok();
+            potomok.Belok = 100.5;
+            potomok.Uglevods = 25;
+            potomok.Zhiri = 73.56;
+
+            Assert.AreEqual(potomok.Q() * 1.5 + potomok.Zhiri * 10, potomok.Qp());
+        }
+    }
+}
